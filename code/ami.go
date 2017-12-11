b30cb4a5af96b73004a50c5345b7f18e3fcabe4e
JJ Merelo <jjmerelo@gmail.com>
2017-12-11 20:35:01 +0100

    No es perfecto, pero funciona

--- code/ami.go
+++ code/ami.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
+
 func main() {
-     gente := [3]string{"Santiago Doc", "Cleri Gorun", "Romi Goget"}
-     copia_gente := gente
-     
+	gente := [3]string{"Santiago Doc", "Cleri Gorun", "Romi Goget"}
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	perm := random.Perm(len(gente))
+	for i, v := range perm {
+		fmt.Printf( "%s → %s\n", gente[v], gente[i] );
+	}
 }
